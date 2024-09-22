93d5d38e9aa8072a4bc1a0b38e56c6dd6434e866
nuryme <hello.nurnahar@gmail.com>
2024-09-23 03:02:53 +0600

    final commit

--- js/common.js
+++ js/common.js
@@ -7,3 +7,17 @@ function gettingEl(id) {
 function gettingElInnerText(id) {
     return parseFloat(document.getElementById(id).innerText);
 }
+
+function toggle(id) {
+    document.getElementById('donate-section').classList.add('hidden');
+    document.getElementById('history-container').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden')
+}
+
+function togglebg(adding, removing) {
+    const addingStyle = document.getElementById(adding);
+    addingStyle.classList.add('bg-primary');
+    const removeStyle = document.getElementById(removing);
+    removeStyle.classList.add('bg-gray-100')
+    removeStyle.classList.remove('bg-primary')
+}
--- js/index.js
+++ js/index.js
@@ -1,19 +1,31 @@
 let noakhaliTotal = 0;
 let remaining = 5500;
-document.getElementById('noakhali-btn').addEventListener('click', function() {
-    const noakhaliInput = parseFloat(document.getElementById('noakhali-input').value);
-    const noakhaliDonation = document.getElementById('noakhali-donation');
-    const total = parseFloat(document.getElementById('total').innerText);
-    
-    if(noakhaliInput > total) {
+document.getElementById('noakhali-btn').addEventListener('click', function () {
+    const noakhaliInput = gettingElValue('noakhali-input');
+    const noakhaliDonation = gettingEl('noakhali-donation');
+    const total = gettingElInnerText('total');
+
+    if (noakhaliInput > total) {
         return alert('You do not have enough money');
     }
-    else if(!isNaN(noakhaliInput) || noakhaliInput > 0) {
+    else if (!isNaN(noakhaliInput) || noakhaliInput > 0) {
+
         noakhaliTotal += noakhaliInput;
         noakhaliDonation.innerText = noakhaliTotal;
         remaining = remaining - noakhaliInput;
         document.getElementById('total').innerText = remaining;
-        return remaining;  
+
+        // history
+        const historyContent = document.getElementById('history-content');
+        const div = document.createElement('div');
+        div.className = ('p-8 bg-white border border-gray-100')
+        div.innerHTML = `
+        <p class="text-xl font-bold">${noakhaliTotal} Taka is Donated for famine-2024 at Noakhali, Bangladesh</p>
+        <p class="text-base font-light">Date: ${new Date().toLocaleDateString()}</p>
+        `
+        historyContent.insertBefore(div, historyContent.firstChild);
+
+        return remaining
     }
     else {
         return alert('invalid Input')
@@ -21,22 +33,77 @@ document.getElementById('noakhali-btn').addEventListener('click', function() {
 })
 
 let feniTotal = 0;
-document.getElementById('feni-btn').addEventListener('click', function() {
-    const feniDonation = document.getElementById('feni-donation');
-    const feniInput = parseFloat(document.getElementById('feni-input').value);
-    const total = parseFloat(document.getElementById('total').innerText);
+document.getElementById('feni-btn').addEventListener('click', function () {
+    const feniDonation = gettingEl('feni-donation');
+    const feniInput = gettingElValue('feni-input');
+    const total = gettingElInnerText('total');
 
-    if(feniInput > total) {
+    if (feniInput > total) {
         return alert('You do not have enough money');
     }
-    else if(!isNaN(feniInput) || feniInput > 0) {
+    else if (!isNaN(feniInput) || feniInput > 0) {
         feniTotal += feniInput;
         feniDonation.innerText = feniTotal;
         remaining = remaining - feniInput;
         document.getElementById('total').innerText = remaining;
-        return remaining;  
+
+        // history
+        const historyContent = document.getElementById('history-content');
+        const div = document.createElement('div');
+        div.className = ('p-8 bg-white border border-gray-100')
+        div.innerHTML += `
+        <p class="text-xl font-bold">${feniTotal} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+        <p class="text-base font-light">Date: ${new Date().toLocaleDateString()}</p>
+        `
+        historyContent.insertBefore(div, historyContent.firstChild);
+
+        return remaining;
     }
     else {
         return alert('invalid Input')
     }
+})
+
+let quotaTotal = 0;
+document.getElementById('quota-btn').addEventListener('click', function () {
+    const quotaDonation = gettingEl('quota-donation');
+    const quotaInput = gettingElValue('quota-input');
+    const total = gettingElInnerText('total');
+
+    if (quotaInput > total) {
+        return alert('You do not have enough money');
+    }
+    else if (!isNaN(quotaInput) || quotaInput > 0) {
+        quotaTotal += quotaInput;
+        quotaDonation.innerText = quotaTotal;
+        remaining = remaining - quotaInput;
+        document.getElementById('total').innerText = remaining;
+
+        // history
+        const historyContent = document.getElementById('history-content');
+        const div = document.createElement('div');
+        div.className = ('p-8 bg-white border border-gray-100')
+        div.innerHTML += `
+        <p class="text-xl font-bold">${quotaTotal} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+        <p class="text-base font-light">Date: ${new Date().toLocaleDateString()}</p>
+        `
+        historyContent.insertBefore(div, historyContent.firstChild);
+
+        return remaining;
+    }
+    else {
+        return alert('invalid Input')
+    }
+})
+
+//history button
+document.getElementById('history-btn').addEventListener('click', function() {
+    toggle('history-container');
+    togglebg('history-btn', 'donation-btn')
+})
+
+//donate button
+document.getElementById('donation-btn').addEventListener('click', function() {
+    toggle('donate-section');
+    togglebg('donation-btn', 'history-btn')
 })
